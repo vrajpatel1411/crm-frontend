d29d9cb44819250e18e0a849f70ec3d34c29d32f
Vraj Patel <vraj.patel.12390@gmail.com>
2024-05-20 18:10:16 -0400

    vercel added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // var url="https://crm-web-api.herokuapp.com";
 // var url="https://crmapiexcel.herokuapp.com" // Paste API URL here.
-var url = "https://crm-backend-qdwwiwmr6-vraj-patels-projects.vercel.app";
+var url = "https://crm-backend-api-rouge.vercel.app/";
 module.exports = { url };
